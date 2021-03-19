ada8c52668c207b86a187daf72013023919b59e3
flowowow <flo.github@gmail.com>
2021-03-19 16:18:35 +0100

    Doener

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 # commiten kann ich nicht so schnell; Pushen allerdings sehr ausdauernd ;)      - Rararasputen, Russians famous love machine
+def zwanzig_doener():
+    for i in range(20):
+        print("Döner")
+zwanzig_doener()

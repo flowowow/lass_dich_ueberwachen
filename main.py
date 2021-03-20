5bce61394de6458fdfd47a4a5701c82016374c68
flowowow <flo.github@gmail.com>
2021-03-20 18:34:06 +0100

    Noch ein bisschen rumprobiert

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ class Test():
 
         return running_processes, stopped_processes
 
-    # Endlose Schleife die in jeder Wiederholung die aktuelle CPU Auslastung ans Ende eines Graphen malt
-    def cpu_stats(self) -> None:
+    # Endlose Schleife die in jeder Wiederholung die aktuelle Takt Frequenz der CPU ans Ende eines Graphen malt
+    def cpu_freq(self) -> None:
         # Variablen deklarieren
         start_time = time.time()
         y = [0]
@@ -36,17 +36,40 @@ class Test():
         fig = plt.figure()
         sub = fig.add_subplot(111)
         sub.plot([1], y, color="blue")
+        plt.title('CPU Frequency')
+        plt.xlabel('frequency in Mhz')
+        plt.xlabel('time in s')
+
+        # Schleife
+        while True:
+            y.append(psu.cpu_freq().current)
+            x.append(round(time.time() - start_time, 2))
+            sub.plot(x, y, color="blue")
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+
+    # Endlose Schleife die in jeder Wiederholung die CPU Auslastung in einem Balkendiagramm aktualisiert
+    def cpu_perc(self) -> None:
+        # Variablen deklarieren
+        x = []
+        y = []
+        for i in range(psu.cpu_count()):
+            x.append(i + 1)
+            y.append(0)
+        
+        # Graph vorzeichnen und betiteln
+        plt.ion()
+        fig = plt.figure()
+        sub = fig.add_subplot(111)
+        sub.bar(x, y, align="center", color="blue")
         plt.title('CPU Workload')
         plt.xlabel('workload in %')
         plt.xlabel('time in s')
 
         # Schleife
         while True:
-            y.append(psu.cpu_percent())
-            x.append(round(time.time() - start_time, 2))
-            sub.plot(x, y, color="blue")
+            for c, i in enumerate(psu.cpu_percent(percpu=True)):
+                y[c] = i
+            sub.bar(x, y, align="center", color="blue")
             fig.canvas.draw()
             fig.canvas.flush_events()
-
-
-Test.cpu_stats(Test)

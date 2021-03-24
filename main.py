eaa63b23a3982c2bf25f62fcff50881636eb717d
flowowow <flo.github@gmail.com>
2021-03-24 18:01:00 +0100

    Reichlich Kommentare in Python Datei hinzugefügt. Für .kv kommt später auch noch was.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import pandas as pd
 import time
 import math
 # "pip install kivy" und "pip install kivymd" zum Importieren
+# https://kivymd.readthedocs.io/en/0.104.0/getting-started.html#installation oder
+# https://kivy.org/doc/stable/gettingstarted/installation.html für eine ausführliche Anleitung
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.snackbar import Snackbar
@@ -17,6 +19,7 @@ from kivy.uix.widget import WidgetException
 
 
 # Probier Klasse --> kann gelöscht werden, wenn wir das nicht brauchen
+# Wird zu Zeit auch nicht verwendet
 class Test():
 
     # Endlose Schleife die in jeder Wiederholung die aktuelle Takt Frequenz der CPU ans Ende eines Graphen malt
@@ -95,6 +98,8 @@ class Test():
 
 
 # Wie eine .kv Datei, bloß als string
+# https://kivy.org/doc/stable/guide/lang.html für mehr Info
+# https://kivy.org/doc/stable/api-kivy.lang.builder.html?highlight=builder# für mehr Info zum Builder
 LIST_ITEM = """
 OneLineIconListItem:
     on_press: app.select_item(self, "OneLineIconListItem")
@@ -109,31 +114,41 @@ class Application(MDApp):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        # Läd .kv Datei
+        # https://kivy.org/doc/stable/api-kivy.lang.builder.html?highlight=builder# für mehr Info
         self.kv = Builder.load_file("KV.kv")
         self.running_processes, self.stopped_processes = self.get_processes()
         self.tracking = False
+        # Snackbar Objekt
+        # https://kivymd.readthedocs.io/en/0.104.0/components/snackbar/index.html für mehr Info
         self.snack1 = Snackbar(text="Select applications to track")
         self.snack2 = Snackbar(text="Turn off tracking to select/deselect applications")
         self.processes_objs_list = []
         self.processes_names_dict = {}
     
+    # build() wird nach __init__() zum Programmstart ausgeführt
     def build(self):
         # Icons werden nicht angezeigt
         menu_items = [{"icon": "filter-outline", "text": "Filtered"}, {"icon": "format-list-bulleted", "text": "Everything"}]
         # Position des Symbols in Toolbar wird nicht aktualisiert, wenn die Fenstergröße verstellt wird --> fix
+        # Dropdown Menü Objekt
+        # https://kivymd.readthedocs.io/en/0.104.0/components/dropdown-item/index.html für mehr Info
         self.filter_menu = MDDropdownMenu(items=menu_items, width_mult=3.5, caller=self.kv.ids.toolbar.children[0], callback=self.filter_menu_close)
         return self.kv
 
+    # on_start wird nach build() zum Programmstart ausgeführt
     def on_start(self):
         self.create_list()
     
     # Für jeden laufenden Process wird ein Listenobjekt erstellt
     def create_list(self) -> None:
         for c, i in enumerate(self.running_processes):
+            # Läd String im .kv Format
             item = Builder.load_string(LIST_ITEM)
             item.text = i
             self.processes_objs_list.append(item)
             self.processes_names_dict[i] = False
+            # Objekt wird mit ID gesucht und zum Renderer hinzugefügt
             self.root.ids.processes.add_widget(self.processes_objs_list[c])
 
     # Gibt zwei Dictionaries zurück, die alle zur Zeit laufenden und gestoppten Prozesse beinhalten
@@ -143,18 +158,26 @@ class Application(MDApp):
         running_processes = {}
         stopped_processes = {}
 
+        # Iteriert durch alle laufenden Prozesse
         for process in psu.process_iter():
             if process.status() == "running":
-                running_processes[process.name()] = process.pid
+                # unnötige .exe Endung wird vom Anwendungsnamen entfernen
+                name = process.name().split(".exe")[0]
+                running_processes[name] = process.pid
             elif process.status() == "stopped":
                 stopped_processes[process.name()] = process.pid
 
         return running_processes, stopped_processes
 
     # Der For-Loop hier funktioniert, ist aber ziemlich ineffizient --> bessere Lösung?
+    # Vielleicht sollte dieses System auch generell nochmal überholt werden
     # Wird aufgerufen wenn ein Listenobjekt gedrückt wird
+    # Ändert Symbol des Listenobjekts und setzt ausgewählten Prozess in Dict auf True
     def select_item(self, instance, position) -> None:
+        # Schaut ob Tracking bereits läuft
         if not self.tracking:
+            # Überprüft durch extra Parameter welches Objekt die Funktion aufgerufen hat
+            # Je nach instance wird entweder Eltern Objekt oder Kind Objekt für Symbol oder Text Änderungen verwendet
             if position == "IconLeftWidget":
                 for i in self.processes_objs_list:
                     if i.text == instance.parent.parent.text:
@@ -173,40 +196,64 @@ class Application(MDApp):
                         elif instance.children[0].children[0].icon == "checkbox-marked-outline":
                             instance.children[0].children[0].icon = "checkbox-blank-outline"
                             self.processes_names_dict[i.text] = False
+        # Wenn Tracking läuft, wird Fehlermeldung über Snackbar angezeigt
+        # https://kivymd.readthedocs.io/en/0.104.0/components/snackbar/index.html für mehr Info
         else:
             try:
                 self.snack2.show()
+            # Bringt Ausnahme hervor, wenn die Snackbar bereits angezeigt wird
             except WidgetException:
                 pass
 
     # Wird ausgeführt wenn der Start Knopf gedrückt wird
+    # Ändert Symbol und Farbe des Knopfes, sowie eine boolean
     def activate_tracking(self, instance):
-        boolean = True
+        block_action = True
 
+        # Wenn keins der Listenobjekte ausgewählt wurde, wird eine Snackbar mit Warnung angezeigt, 
+        # Sowie die weiteren Änderungen übersprungen
         for i in self.processes_names_dict:
             if self.processes_names_dict[i]:
-                boolean = True
+                block_action = False
                 break
         else:
-            boolean = False
+            block_action = True
+            # Snackbar
+            # https://kivymd.readthedocs.io/en/0.104.0/components/snackbar/index.html für mehr Info
             try:
                 self.snack1.show()
+            # Bringt Ausnahme hervor, wenn die Snackbar bereits angezeigt wird
             except WidgetException:
                 pass
         
-        if boolean:
+        if not block_action:
+            # Überprüft anhand der Farbe des Knopfes, ob Tracking läuft oder nicht
+            # Das ist etwas gay, funktioniert aber
             if instance.md_bg_color == self.theme_cls.primary_color:
+                # boolean = True
                 self.tracking = True
+                # Symbol des Knopfes ändern
                 instance.icon = "stop"
-                instance.md_bg_color = (.79, .22, .29, 1)
+                # Farbe des Knopfes ändern (prozentualer rbga Wert als Dezimalzahl)
+                instance.md_bg_color = (.79, .22, .29, 1) # --> Rot
             else:
+                # boolean = False
                 self.tracking = False
+                # Symbol des Knopfes ändern
                 instance.icon = "play"
-                instance.md_bg_color = self.theme_cls.primary_color
+                # Farbe des Knopfes ändern (standard Farbe)
+                instance.md_bg_color = self.theme_cls.primary_color # --> Blau
 
+    # Schließt Dropdown Menü
+    # Hier muss später die Variable noch direkt an eine Aktualisierungsfunktion der Liste weitergegeben werden
     def filter_menu_close(self, instance):
-        print(instance.text)
+        print(instance.text) # Bloß temporär zum Debuggen
+        # MDDropdownMenu.dismiss() schließt das Dropdown Menü
+        # https://kivymd.readthedocs.io/en/0.104.0/components/dropdown-item/index.html für mehr Info
         self.filter_menu.dismiss()
 
-
+# Führt Application Klasse aus
+# Klasse akzeptiert beim Aufruf keine Argumente und gibt eine neue Instanz ohne Features zurück,
+# die keine Instanzattribute hat und keine annehmen kann.
+# https://kivy.org/doc/stable/api-kivy.app.html für mehr Info
 Application().run()
